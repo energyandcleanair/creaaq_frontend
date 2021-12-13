ff39cd7a94ec2831ee3539318e25b1177d5b31ad
Max Martynov <martynov.max.s@gmail.com>
2021-12-13 13:33:27 +0300

    GTM. Update the config to track chaning of views properly

--- src/plugins/analytics/gtm.ts
+++ src/plugins/analytics/gtm.ts
@@ -1,10 +1,17 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueRouter, {Route} from 'vue-router'
 import VueGtm from '@gtm-support/vue2-gtm'
 import config from '@/config'
 import {trackGtmEvent} from './trackGtmEvent'
 
 export const initGtmAnalytics = (router: VueRouter) => {
+  if (config.get('GTM_ENABLED') === 'true' && !config.get('GTM_CONTAINER_ID')) {
+    console.error(
+      "Google Tag Manager plugin won't be initialized. Environment variable 'GTM_CONTAINER_ID' is not defined."
+    )
+    return
+  }
+
   Vue.use(VueGtm, {
     id: config.get('GTM_CONTAINER_ID'),
 
@@ -24,7 +31,7 @@ export const initGtmAnalytics = (router: VueRouter) => {
     loadScript: true,
 
     // Pass the router instance to automatically sync with router (optional)
-    vueRouter: router,
+    vueRouter: undefined,
 
     // Don't trigger events for specified router names (optional)
     ignoredViews: [],
@@ -34,4 +41,51 @@ export const initGtmAnalytics = (router: VueRouter) => {
   })
 
   Vue.prototype.$trackGtmEvent = Vue.trackGtmEvent = trackGtmEvent
+
+  // track unique view changes
+  router.afterEach((to: Route, from: Route) => {
+    if (typeof to.name !== 'string') return
+
+    // this is how you will see this record in the Vue.dataLayer array
+    const data: DataLayerObjectView = {
+      event: 'content-view',
+      'content-view-name': to.name,
+      'content-name': to.path,
+    }
+
+    const lastData = _getGTMLastDataLayerItem('event', 'content-view')
+    const isSameView =
+      lastData?.event === 'content-view' && lastData['content-name'] === to.path
+
+    if (!Vue.gtm.enabled() || !to.name || isSameView) return
+    Vue.gtm.trackView(data['content-view-name'], data['content-name'])
+  })
+}
+
+type DataLayerContentViewName = string
+type DataLayerContentViewPath = string
+interface DataLayerObjectView {
+  event: 'content-view'
+  'content-view-name': DataLayerContentViewName
+  'content-name': DataLayerContentViewPath
+}
+
+function _getGTMLastDataLayerItem(
+  key: string,
+  value: string
+): DataLayerObjectView | null {
+  const dataLayer = Vue.gtm.dataLayer()
+  let resultItem: DataLayerObjectView | null = null
+
+  if (dataLayer === false) return resultItem
+
+  let i = dataLayer.length
+  while (i--) {
+    if (dataLayer[i][key] === value) {
+      resultItem = dataLayer[i] as DataLayerObjectView
+      break
+    }
+  }
+
+  return resultItem
 }
--- src/plugins/analytics/trackGtmEvent.ts
+++ src/plugins/analytics/trackGtmEvent.ts
@@ -1,13 +1,31 @@
 import Vue from 'vue'
 import {TrackEventOptions} from '@gtm-support/vue2-gtm'
 
+/**
+ * Track an event.
+ *
+ * The event will only be send if the script runs in browser context and the if plugin is enabled.
+ *
+ * If debug mode is enabled, a "Dispatching event" is logged,
+ * regardless of whether the plugin is enabled or the plugin is being executed in browser context.
+ *
+ * @param category passed as `target`.
+ * @param action passed as `action`.
+ * @param label passed as `"target-properties"`.
+ * @param rest
+ * @param rest.value Optional passed as `value`.
+ * @param rest.noninteraction Optional `noninteraction`, passed as `"interaction-type"`.
+ */
 export const trackGtmEvent: ITrackGtmEventHandler = (
   category: unknown,
   action: unknown,
   label: unknown,
   rest?: TrackEventOptions
 ): void => {
+  if (!Vue.gtm.enabled()) return
+
   const opts: TrackEventOptions = {
+    event: rest?.event ?? 'interaction',
     category,
     action,
     label,
