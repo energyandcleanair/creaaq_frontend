176112b68e18347a6c104f77a28397b7c99cde5a
Oscar <druiventakoscar@gmail.com>
2024-01-25 13:48:04 +0100

    disable stadia basemap

--- src/views/trajectory/components/TrajectoryMap.vue
+++ src/views/trajectory/components/TrajectoryMap.vue
@@ -84,12 +84,12 @@ export default class TrajectoryMap extends Vue {
           attribution:
             'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
         },
-        {
-          name: 'Stadia.AlidadeSmoothDark', 
-          url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
-          visible: false,
-          attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }
+        // {
+        //   name: 'Stadia.AlidadeSmoothDark', 
+        //   url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+        //   visible: false,
+        //   attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        // }
       ]
 
 
